127ef462a2c97081f3dbbda55370360bac97d819
ainhoa3 <150441702+ainhoa3@users.noreply.github.com>
2025-06-02 21:08:21 +0200

    Update Program.cs

--- DailyFlow/Program.cs
+++ DailyFlow/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigins",
     policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://localhost:4200")
               .AllowCredentials()
               .AllowAnyMethod()
               .AllowAnyHeader();
